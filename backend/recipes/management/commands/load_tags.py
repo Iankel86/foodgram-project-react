df799e721d9f8f5fd8ca7694f0d0a35ffe6edcb9
Ян Шадрин <Yankelll86@mail.ru>
2022-10-15 15:56:22 +0400

    Добавил main

--- backend/recipes/management/commands/load_tags.py
+++ backend/recipes/management/commands/load_tags.py
@@ -19,3 +19,15 @@ class Command(BaseCommand):
         self.stdout.write(
             self.style.SUCCESS("Теги загружены в БД!")
         )
+
+
+        # data = [
+        #     {"name": "Горячие блюда", "color": "#F70B07", "slug": "hot dishes"},
+        #     {"name": "Салаты", "color": "#F9FC23", "slug": "salads"},
+        #     {"name": "Закуски", "color": "#0FF225", "slug": "snacks"},
+        #     {"name": "Супы", "color": "#D3E88E", "slug": "soups"},
+        #     {"name": "Выпечка", "color": "#FAC13C", "slug": "bakery products"},
+        #     {"name": "Десерты", "color": "#F51128", "slug": "desserts"},
+        #     {"name": "Напитки", "color": "#1CE8DE", "slug": "drinks"},
+        #     {"name": "Детские блюда", "color": "#D01CE8", "slug": "children's meals"},
+        # ]
