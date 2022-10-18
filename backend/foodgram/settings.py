7f36c4930211cd059ad061f451ce17b914df0f70
Ян Шадрин <Yankelll86@mail.ru>
2022-10-18 12:59:43 +0400

    коррекция №17

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -58,12 +58,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "foodgram.wsgi.application"
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 DATABASES = {
     'default': {
